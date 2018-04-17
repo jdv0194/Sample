818c60c7b6d1df3f9e30a498bdcab298188e7382
Alex Scarlett <alex.scarlett91@gmail.com>
2018-04-17 17:22:47 -0400

    Added jquery to front page, changed button scroll and size of hours section

---
+++ assets/js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  // FADE IN OPENING TEXT
+  $('.header, .header-p, .header-text button, hr').hide();
+
+  $('.header, hr')
+    .delay(500)
+    .show(1200);
+
+  $('.header-p')
+    .delay(2000)
+    .fadeIn(1200);
+
+  $('.header-text button')
+    .delay(3400)
+    .fadeIn(1000);
+
+  // SCROLL TO ABOUT
+  $('.header-text button').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('#about-us').offset().top
+      },
+      750
+    );
+  });
+
+  // SCROLL TO HOURS/MENU SECTION
+  $('.about-header button').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.hours-open').offset().top
+      },
+      750
+    );
+  });
+});
